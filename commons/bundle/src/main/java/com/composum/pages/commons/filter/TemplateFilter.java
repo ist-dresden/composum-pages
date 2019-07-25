b52160043b9f313b11290e991c02aef168d040be
Ralf Wunsch <r.wunsch@ist-software.com>
2019-07-25 16:26:42 +0200

    set up usage as template for preconfigured pages in site templates

--- commons/bundle/src/main/java/com/composum/pages/commons/filter/TemplateFilter.java
+++ commons/bundle/src/main/java/com/composum/pages/commons/filter/TemplateFilter.java
@@ -3,12 +3,15 @@ package com.composum.pages.commons.filter;
 import com.composum.sling.core.filter.ResourceFilter;
 import org.apache.jackrabbit.JcrConstants;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ValueMap;
 
-import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.composum.pages.commons.PagesConstants.PROP_IS_TEMPLATE;
 import static com.composum.pages.commons.PagesConstants.PROP_TEMPLATE;
+import static com.composum.pages.commons.PagesConstants.PROP_TEMPLATE_REF;
 
 /**
  * accepts page resources marked as a template
@@ -18,17 +21,32 @@ public class TemplateFilter extends ResourceFilter.AbstractResourceFilter {
     public static final TemplateFilter INSTANCE = new TemplateFilter();
 
     @Override
-    public boolean accept(@Nonnull final Resource resource) {
-        Resource content = resource.getChild(JcrConstants.JCR_CONTENT);
-        if (content != null) {
+    public boolean accept(Resource resource) {
+        boolean isTemplate = true;
+        List<String> trace = new ArrayList<>();
+        Resource content;
+        while (isTemplate && resource != null && !trace.contains(resource.getPath())
+                && (content = resource.getChild(JcrConstants.JCR_CONTENT)) != null) {
+            trace.add(resource.getPath());
             ValueMap values = content.getValueMap();
-            Boolean isTemplate = values.get(PROP_IS_TEMPLATE, Boolean.class);
-            if (isTemplate == null) {
-                isTemplate = values.get(PROP_TEMPLATE, String.class) == null;
+            Boolean isTemplateProp = values.get(PROP_IS_TEMPLATE, Boolean.class);
+            if (isTemplateProp == null) {
+                String templatePath = values.get(PROP_TEMPLATE, String.class);
+                if (templatePath != null) {
+                    ResourceResolver resolver = resource.getResourceResolver();
+                    resource = resolver.getResource(templatePath);
+                    continue;
+                }
+                templatePath = values.get(PROP_TEMPLATE_REF, String.class);
+                if (templatePath != null) {
+                    ResourceResolver resolver = resource.getResourceResolver();
+                    resource = resolver.getResource(templatePath);
+                }
+            } else {
+                isTemplate = isTemplateProp;
             }
-            return isTemplate;
         }
-        return false;
+        return isTemplate;
     }
 
     @Override
