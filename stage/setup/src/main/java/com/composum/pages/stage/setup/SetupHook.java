9d1cba8b94a7deb8a49d2d6edc41be0e28194e5b
Ralf Wunsch <r.wunsch@ist-software.com>
2019-08-20 08:37:48 +0200

    Pages 'homepage' moved analogous to Platform, setup configuration for starter fixed

--- stage/setup/src/main/java/com/composum/pages/stage/setup/SetupHook.java
+++ stage/setup/src/main/java/com/composum/pages/stage/setup/SetupHook.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
 import javax.jcr.Property;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
@@ -15,18 +16,21 @@ public class SetupHook implements InstallHook {
 
     private static final Logger LOG = LoggerFactory.getLogger(SetupHook.class);
 
-    private static final String PAGES_STAGE_INDEX = "/conf/composum/pages/stage/home.html";
-    private static final String DEFAULT_SLING_INDEX = "/index.html";
+    private static final String PAGES_STAGE_INDEX = "/libs/composum/pages/home.html";
+    private static final String DEFAULT_SLING_INDEX = "/starter/index.html";
 
+    private static final String TYPE_SLING_REDIRECT = "sling:redirect";
+
+    private static final String PROP_SLING_RESOURCE_TYPE = "sling:resourceType";
     private static final String PROP_SLING_TARGET = "sling:target";
 
     @Override
-    public void execute(InstallContext ctx) {
+    public void execute(InstallContext ctx) throws PackageException {
         //noinspection SwitchStatementWithTooFewBranches
         switch (ctx.getPhase()) {
             case INSTALLED:
                 LOG.info("installed: execute...");
-                //setupIndexPage(ctx);
+                setupIndexPage(ctx);
                 LOG.info("installed: execute ends.");
         }
     }
@@ -35,15 +39,26 @@ public class SetupHook implements InstallHook {
         try {
             Session session = ctx.getSession();
             Node rootNode = session.getNode("/");
-            Property redirect = rootNode.getProperty(PROP_SLING_TARGET);
-            if (redirect != null && DEFAULT_SLING_INDEX.equals(redirect.getString())) {
-                // replace the redirect target only if this property is not changed (is the original 'Sling' value)
-                rootNode.setProperty(PROP_SLING_TARGET, PAGES_STAGE_INDEX);
-                session.save();
-            }
+            setPropertyIfNotChanged(rootNode, PROP_SLING_RESOURCE_TYPE, TYPE_SLING_REDIRECT, null);
+            setPropertyIfNotChanged(rootNode, PROP_SLING_TARGET, PAGES_STAGE_INDEX, DEFAULT_SLING_INDEX);
+            session.save();
         } catch (RepositoryException | RuntimeException rex) {
             LOG.error(rex.getMessage(), rex);
             throw new PackageException(rex);
+        }
+    }
+
+    protected void setPropertyIfNotChanged(Node node, String name, String value, String def)
+            throws RepositoryException {
+        try {
+            Property current = node.getProperty(name);
+            if (def == null || def.equals(current.getString())) {
+                // replace the property only if not changed (is the original default value)
+                node.setProperty(name, value);
+            }
+        } catch (PathNotFoundException ignore) {
+            // no target property set...
+            node.setProperty(name, value);
         }
     }
 }
