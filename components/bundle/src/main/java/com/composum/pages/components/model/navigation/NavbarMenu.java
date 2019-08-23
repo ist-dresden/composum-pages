2abf413f9e5488cbde58cd5931568d84f7f013f4
Ralf Wunsch <r.wunsch@ist-software.com>
2019-08-23 10:13:46 +0200

    single line navbar - fix for the 'meta' pages

--- components/bundle/src/main/java/com/composum/pages/components/model/navigation/NavbarMenu.java
+++ components/bundle/src/main/java/com/composum/pages/components/model/navigation/NavbarMenu.java
@@ -1,5 +1,7 @@
 package com.composum.pages.components.model.navigation;
 
+import com.composum.pages.commons.model.Page;
+import com.composum.pages.commons.model.Site;
 import com.composum.sling.core.BeanContext;
 import org.apache.sling.api.resource.Resource;
 
@@ -39,8 +41,21 @@ public class NavbarMenu extends Menu {
         if (menuItems == null) {
             menuParent = resource;
             do {
-                menuItems = buildMenuItems(menuParent);
-            } while (menuItems.size() < 1 && (menuParent = menuParent.getParent()) != null);
+                if (Site.isSite(menuParent)) {
+                    Site site = getSiteManager().getContainingSite(context, menuParent);
+                    if (site != null) {
+                        Page homepage = site.getHomepage(getLocale());
+                        if (homepage != null) {
+                            menuParent = homepage.getResource();
+                            menuItems = buildMenuItems(menuParent);
+                            break;
+                        }
+                    }
+                }
+                if (Page.isPage(menuParent)) {
+                    menuItems = buildMenuItems(menuParent);
+                }
+            } while ((menuItems == null || menuItems.size() < 1) && (menuParent = menuParent.getParent()) != null);
         }
         return menuItems;
     }
