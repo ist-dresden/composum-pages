7b41a9bbde59ddfe5d9a2a61eff976b79ad3c5ee
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2019-12-05 14:54:35 +0100

    also publish asset configurations in releases

--- options/assets/bundle/src/main/java/com/composum/pages/options/assets/service/AssetsVersionFactory.java
+++ options/assets/bundle/src/main/java/com/composum/pages/options/assets/service/AssetsVersionFactory.java
@@ -1,10 +1,13 @@
 package com.composum.pages.options.assets.service;
 
 import com.composum.assets.commons.AssetsConstants;
+import com.composum.assets.commons.config.AssetConfig;
 import com.composum.pages.commons.model.ContentVersion;
 import com.composum.pages.commons.model.SiteRelease;
 import com.composum.pages.commons.service.VersionFactory;
+import com.composum.pages.options.assets.api.AssetContent;
 import com.composum.pages.options.assets.api.AssetVersion;
+import com.composum.sling.core.util.ResourceUtil;
 import com.composum.sling.platform.staging.versions.PlatformVersionsService;
 import org.apache.sling.api.resource.Resource;
 import org.osgi.framework.Constants;
@@ -23,14 +26,20 @@ public class AssetsVersionFactory implements VersionFactory {
 
     @Override
     @Nullable
-    public ContentVersion getContentVersion(@Nonnull final SiteRelease siteRelease,
-                                            @Nonnull final Resource contentResource, @Nonnull final String type,
-                                            @Nonnull final PlatformVersionsService.Status status) {
+    public ContentVersion<AssetContent> getContentVersion(@Nonnull final SiteRelease siteRelease,
+                                                          @Nonnull final Resource contentResource, @Nonnull final String type,
+                                                          @Nonnull final PlatformVersionsService.Status status) {
+        boolean isAssetsRelated;
         switch (type) {
             case AssetsConstants.NODE_TYPE_ASSET:
             case AssetsConstants.NODE_TYPE_ASSET_CONTENT:
-                return new AssetVersion(siteRelease, status);
+                isAssetsRelated = true;
+                break;
+            default:
+                Resource assetsConfig = contentResource.getChild(AssetConfig.CHILD_NAME);
+                isAssetsRelated = ResourceUtil.isNodeType(assetsConfig, AssetsConstants.NODE_TYPE_ASSET_CONFIG);
+                break;
         }
-        return null;
+        return isAssetsRelated ? new AssetVersion(siteRelease, status) : null;
     }
 }
