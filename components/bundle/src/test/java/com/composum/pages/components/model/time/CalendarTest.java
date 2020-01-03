d002429682209da7cf21df9b790d92916d38fb1f
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2020-01-03 08:00:23 +0100

    fix CalendarTest: use of SimpleDateFormat without a locale

--- components/bundle/src/test/java/com/composum/pages/components/model/time/CalendarTest.java
+++ components/bundle/src/test/java/com/composum/pages/components/model/time/CalendarTest.java
@@ -1,5 +1,6 @@
 package com.composum.pages.components.model.time;
 
+import com.composum.sling.core.BeanContext;
 import org.apache.commons.lang3.LocaleUtils;
 import org.junit.Assert;
 import org.junit.Ignore;
@@ -9,6 +10,8 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
+
+import static com.composum.pages.commons.PagesConstants.RA_STICKY_LOCALE;
 
 public class CalendarTest {
 
@@ -24,6 +27,11 @@ public class CalendarTest {
 
     protected class TestCalendar extends CalendarModel {
 
+        public TestCalendar(Locale locale) {
+            context = new BeanContext.Service();
+            context.setAttribute(RA_STICKY_LOCALE, locale, BeanContext.Scope.page);
+        }
+
         @Override
         public int getColumns() {
             return 1;
@@ -37,11 +45,12 @@ public class CalendarTest {
 
     @Test
     public void testLocaleDe() {
-        Calendar time = getTime(LOCALE_DE);
-        CalendarModel calendar = new TestCalendar();
-        calendar.setRange(new DateRange(null, LOCALE_DE,
+        Locale locale = LOCALE_DE;
+        Calendar time = getTime(locale);
+        CalendarModel calendar = new TestCalendar(locale);
+        calendar.setRange(new DateRange(null, locale,
                 Calendar.MONTH, time.get(Calendar.YEAR), time.get(Calendar.MONTH) + 1, 3));
-        System.out.println(LOCALE_DE.toString());
+        System.out.println(locale.toString());
         String result = printCalendar(calendar);
         Assert.assertTrue(result.contains("2019/09: 40  _30_  01   02   03   04   05   06"));
         Assert.assertTrue(result.contains("2019/11: 47   18   19   20   21   22   23   24"));
@@ -50,29 +59,30 @@ public class CalendarTest {
 
     @Test
     public void testLocaleUs() {
-        Calendar time = getTime(LOCALE_US);
-        CalendarModel calendar = new TestCalendar();
-        calendar.setRange(new DateRange(null, LOCALE_US,
+        Locale locale = LOCALE_US;
+        Calendar time = getTime(locale);
+        CalendarModel calendar = new TestCalendar(locale);
+        calendar.setRange(new DateRange(null, locale,
                 Calendar.MONTH, time.get(Calendar.YEAR), time.get(Calendar.MONTH) + 1, 3));
-        System.out.println(LOCALE_US.toString());
+        System.out.println(locale.toString());
         String result = printCalendar(calendar);
-        Assert.assertTrue(result.contains("2019/09: 39  _29_ _30_  01   02   03   04   05"));
-        Assert.assertTrue(result.contains("2019/10: 43  _27_ _28_ _29_ _30_ _31_  01   02"));
-        Assert.assertTrue(result.contains("2019/12: 48   01   02   03   04   05   06   07"));
+        Assert.assertTrue(result.contains("2019/09: 40  _29_ _30_  01   02   03   04   05"));
+        Assert.assertTrue(result.contains("2019/10: 44  _27_ _28_ _29_ _30_ _31_  01   02"));
+        Assert.assertTrue(result.contains("2019/12: 49   01   02   03   04   05   06   07"));
     }
 
     @Test
     public void testLocaleAe() {
         Locale locale = LocaleUtils.toLocale("ar_AE");
         Calendar time = getTime(locale);
-        CalendarModel calendar = new TestCalendar();
+        CalendarModel calendar = new TestCalendar(locale);
         calendar.setRange(new DateRange(null, locale,
                 Calendar.MONTH, time.get(Calendar.YEAR), time.get(Calendar.MONTH) + 1, 3));
         System.out.println(locale.toString());
         String result = printCalendar(calendar);
-        Assert.assertTrue(result.contains("2019/09: 39  _28_ _29_ _30_  01   02   03   04"));
-        Assert.assertTrue(result.contains("2019/10: 43  _26_ _27_ _28_ _29_ _30_ _31_  01"));
-        Assert.assertTrue(result.contains("2019/12: 52   28   29   30   31  _01_ _02_ _03_"));
+        Assert.assertTrue(result.contains("2019/09: 40  _28_ _29_ _30_  01   02   03   04"));
+        Assert.assertTrue(result.contains("2019/10: 44  _26_ _27_ _28_ _29_ _30_ _31_  01"));
+        Assert.assertTrue(result.contains("2019/12: 01   28   29   30   31  _01_ _02_ _03_"));
     }
 
     public <Type extends TimeRelated> String printCalendar(CalendarModel calendar) {
@@ -85,11 +95,11 @@ public class CalendarTest {
                 for (CalendarModel.Month.Day day : month) {
                     Calendar date = day.getDate();
                     if (day.isFirstDayOfWeek()) {
-                        builder.append(new SimpleDateFormat("\nyyyy/MM: ww ").format(date.getTime()));
+                        builder.append(new SimpleDateFormat("\nyyyy/MM: ww ", calendar.getLocale()).format(date.getTime()));
                     }
                     builder.append(" ")
                             .append(day.isDayOfMonth() ? " " : "_")
-                            .append(new SimpleDateFormat("dd").format(date.getTime()))
+                            .append(new SimpleDateFormat("dd", calendar.getLocale()).format(date.getTime()))
                             .append(day.isDayOfMonth() ? " " : "_");
                     index++;
                 }
